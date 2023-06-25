a4072ad6a5b0888a3b21b4dd48c7c895b7ae7348
subham-k <subhamkumar5375@gmail.com>
2023-06-25 21:56:25 +0530

    Adding implicit intent file

--- app/src/main/java/com/example/explicitintent/MainActivity.kt
+++ app/src/main/java/com/example/explicitintent/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.explicitintent
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.webkit.WebStorage
 import android.widget.Button
+import androidx.cardview.widget.CardView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +19,20 @@ class MainActivity : AppCompatActivity() {
         buttonNext.setOnClickListener {
 
             intent= Intent(applicationContext,SecondActivity::class.java)
+            startActivity(intent)
+        }
+
+        val webButton= findViewById<CardView>(R.id.cardGoogle)
+        val cameraButton= findViewById<CardView>(R.id.cardCamera)
+
+        webButton.setOnClickListener {
+            val intent= Intent(Intent.ACTION_VIEW)
+            intent.data= Uri.parse("https://leetcode.com/subhamkumar75/")
+            startActivity(intent)
+        }
+
+        cameraButton.setOnClickListener {
+            val intent= Intent(MediaStore.ACTION_IMAGE_CAPTURE)
             startActivity(intent)
         }
     }
